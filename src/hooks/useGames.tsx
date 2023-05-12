2b98ec645669c7364f0742d9ee9f4d05f8b6979f
Jatin <rathorejatin168@gmail.com>
2023-05-12 20:10:02 +0530

    Displaying platform icons

--- src/hooks/useGames.tsx
+++ src/hooks/useGames.tsx
@@ -2,10 +2,17 @@ import { useState, useEffect } from "react";
 import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 
+export interface Platform {
+  id: number;
+  name: string;
+  slug: string;
+}
+
 export interface Game {
   id: number;
   name: string;
   background_image: string;
+  parent_platforms: { platform: Platform }[];
 }
 
 interface FetchGamesResponse {
@@ -22,7 +29,10 @@ const useGames = () => {
 
     apiClient
       .get<FetchGamesResponse>("games", { signal: controller.signal })
-      .then((res) => setGames(res.data.results))
+      .then((res) => {
+        console.log(res.data.results);
+        setGames(res.data.results);
+      })
       .catch((err) => {
         if (err instanceof CanceledError) return;
         serError(err.message);
