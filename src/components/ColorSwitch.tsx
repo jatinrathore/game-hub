faaa9629bb7f42eefa3737b7fc09e667e04b992c
Jatin <rathorejatin168@gmail.com>
2023-05-12 14:48:10 +0530

    Fetching the games

--- src/components/ColorSwitch.tsx
+++ src/components/ColorSwitch.tsx
@@ -2,6 +2,7 @@ import { HStack, Switch, Text, useColorMode } from "@chakra-ui/react";
 
 const ColorSwitch = () => {
   const { toggleColorMode, colorMode } = useColorMode();
+
   return (
     <HStack>
       <Switch
