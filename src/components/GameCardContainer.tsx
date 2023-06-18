8baff3c35a3ad31b472048bd6fe19b43e7ce8ad3
Jatin <rathorejatin168@gmail.com>
2023-06-18 13:49:03 +0530

    Fetch game details

--- src/components/GameCardContainer.tsx
+++ src/components/GameCardContainer.tsx
@@ -1,12 +1,17 @@
-import { Box } from "@chakra-ui/react";
-import React, { ReactNode } from "react";
+import { Box, transition } from "@chakra-ui/react";
+import { ReactNode } from "react";
 
 interface Props {
   children: ReactNode;
 }
 const GameCardContainer = ({ children }: Props) => {
   return (
-    <Box borderRadius={10} overflow="hidden">
+    <Box
+      borderRadius={10}
+      overflow="hidden"
+      _hover={{ transform: "scale(1.03)" }}
+      transition={"transform .15s ease-in"}
+    >
       {children}
     </Box>
   );
--- src/hooks/useGames.tsx
+++ src/hooks/useGames.tsx
@@ -6,6 +6,8 @@ import { Platform } from "./usePlatform";
 export interface Game {
   id: number;
   name: string;
+  slug: string;
+  description_raw: string;
   background_image: string;
   parent_platforms: { platform: Platform }[];
   metacritic: number;
